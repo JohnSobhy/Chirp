9bf33706e97dae8d833a79efb2f6c650f6ad4bb1
john.sobhy <john.sobhy@inspirehub.net>
2025-10-28 18:16:39 +0300

    Result class

---
+++ core/domain/src/commonMain/kotlin/com/john_halaka/core/domain/util/DataError.kt
@@ -0,0 +1,26 @@
+package com.john_halaka.core.domain.util
+
+sealed interface DataError: Error {
+    enum class Remote: DataError {
+        BAD_REQUEST,
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        CONFLICT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        SERVER_ERROR,
+        SERVICE_UNAVAILABLE,
+        SERIALIZATION_ERROR,
+        PAYLOAD_TOO_LARGE,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL,
+        FILE_NOT_FOUND,
+        UNKNOWN
+    }
+}
+
---
+++ core/domain/src/commonMain/kotlin/com/john_halaka/core/domain/util/Error.kt
@@ -0,0 +1,3 @@
+package com.john_halaka.core.domain.util
+
+interface Error
